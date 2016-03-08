62e29cb21e2b4aa45ba89a54d5152613f0a9853d
刘洋 <liuyang_06_01@163.com>
2016-03-08 20:47:09 +0800

    哎呀，不错哦。可以对比线上了

--- funny/src/main/java/com/cn/local/funny/model/User.java
+++ funny/src/main/java/com/cn/local/funny/model/User.java
@@ -1,5 +1,37 @@
 package com.cn.local.funny.model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -2396089073399244168L;
+
+	private Integer id;
+	private String username;
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
